d3c489070a0358ddb4d3eeda61f9e1a1355e5796
vinnylav <vinnylav@mac.com>
2023-04-03 19:30:35 -0400

    new plan

--- Scripts/Game/CharacterSelect.gd
+++ Scripts/Game/CharacterSelect.gd
@@ -5,20 +5,21 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-var orchero = "res://Scripts/Hero/Orc_Rig.tscn"
-var piratehero = "res://Scripts/Hero/Pirate_Rig.tscn"
+var orcH = "res://Scripts/Hero/Orc_Rig.tscn"
+var pirateH = "res://Scripts/Hero/Pirate_Rig.tscn"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	match Heros.characterOptions:
-		0:
-			$UI/HeroHolder.add_child(orchero)
-		1:
-			$UI/HeroHolder.add_child(piratehero)
+	pass
 
 
 func _on_next_button_pressed():
-	$UI/HeroHolder.add_child(orchero)
+	pass
 
 
 func _on_previous_button_pressed():
-	$UI/HeroHolder.add_child(piratehero)
+	pass
+
+
+func _on_select_hero_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Zone1/Zone1.tscn")
+	pass # Replace with function body.
