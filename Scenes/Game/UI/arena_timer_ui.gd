183e4422d5a5fe383bd7c12c58e3c8a3c79aeb10
vinnylav <vinnylav@mac.com>
2023-04-04 23:25:09 -0400

    Timer Fix

--- Scenes/Game/UI/arena_timer_ui.gd
+++ Scenes/Game/UI/arena_timer_ui.gd
@@ -1,18 +1,18 @@
 extends CanvasLayer
 
 @export var arena_time_manager: Node
-@onready var label = %Label
-
+@onready var label = $%Label
 
 func _process(delta):
 	if arena_time_manager == null:
 		return
 	var time_elapsed = arena_time_manager.get_time_elapsed()
-	label.text = str(time_elapsed)
-	#label.text = format_seconds_to_string(time_elapsed)
+	#label.text = str(time_elapsed)
+	label.text = format_seconds_to_string(time_elapsed)
+
 
 func format_seconds_to_string(seconds: float):
 	var minutes = floor(seconds / 60)
 	var remaining_seconds = seconds - (minutes * 60)
 	#enforce 2 digit seconds
-	return str(minutes) + ":" + ("%02d" %(floor(remaining_seconds))
+	return str(minutes) + ":" + ("%02d" % floor(remaining_seconds))
